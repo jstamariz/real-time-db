cac4445efaa134f2fa89385b98c6db5813482b04
Jorge Tamariz <jtamariz@flexiti.com>
2024-04-01 14:22:01 -0400

    Implement endpoint and add classes to DI

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using real_time_db.Db;
+using real_time_db.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<FooContext>();
+builder.Services.AddTransient<FooContext>();
+builder.Services.AddSingleton<FooService>();
 
 var app = builder.Build();
 
@@ -19,10 +26,14 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapPost("/write", () => {
-
+app.MapPost("/write", (FooService service) => {
+    service.Write();
+    return Results.Created();
 });
 
-app.MapGet("/read", () => {
-
+app.MapGet("/read", (FooService service) => {
+    var result  = service.Read();
+    return Results.Ok(result);
 });
+
+app.Run();
